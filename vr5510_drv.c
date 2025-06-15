40db7107988c7a5b7807458f9f451ba5a8b22162
jerrywang202506 <jerry.wangrx@aliyun.com>
2025-06-15 13:07:36 +0800

    Create vr5510_drv.c

---
+++ vr5510_drv.c
@@ -0,0 +1,34 @@
+/*vr5510_drv.c*/
+/*
+*Features
+*Key Features
+*   Four high-efficiency BUCK regulators – two low voltage bucks support multi-phase and SVS/DVS capability
+*   Three LDO regulators – two LDOs with load switch option
+*   One low power, high voltage LDO
+*   One BOOST regulator with integrated low side switch
+*   56-pin 8 x 8 QFN package
+*
+*System and Device Features
+*   60 V maximum input voltage for automotive and truck applications
+*   Standby mode with very low quiescent current
+*   2x input pins for wake-up detection and battery voltage sense
+*   OTP (One-time programmable) memory for flexible device configuration
+*   High speed I²C interface (3.4 MHz)
+*   External clock synchronization, Frequency spread spectrum, and manual frequency tuning
+*   
+*Safety Features to fit ASIL D Safety Level
+*   Independent circuits to fit ASIL D safety level
+*   Voltage monitoring with programmable fault protection
+*   Analog built-in self-test (ABIST) and Logical built-in self-test (LBIST)
+*   Simple and Challenger watchdog
+*   Dedicated MCU monitoring interface
+*   PGOOD, RESET, Fail-Safe outputs
+*   Advanced thermal monitoring and protection
+*   
+*Programmability
+*   Supplies, sequencing, and specific functions can be programmed by One Time programmable (OTP) memory
+*   Standard Predefined OTP supporting popular MCUs can be found in data sheet “ordering information” chapter
+*   During development phase, Customized OTP part can be programmed using the KITVR5510SKTEVM board
+*   Custom OTP can be provided for production. Please contact your NXP sales channel (NXP or your distributor)
+*   Custom OTP can be created using the VR5510 OTP tool Integrated in the NXP GUI Software tool
+*/
